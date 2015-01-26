3acff57095888f2001a8d4c3be72558d86f9cfd4
Olga <olga.shcherb@gmail.com>
2015-01-26 23:52:57 +0100

    added releases 1.5 and 1.6; seamark and bitcoin maps article.

--- website/scripts/blog.js
+++ website/scripts/blog.js
@@ -2,9 +2,9 @@ var blogArticles = [
 	{title:'OsmAnd 1.9', url:'blog.html?id=osmand-1-9-released', index: 0},
 	{title:'OsmAnd 1.8', url:'#', index: 1},
 	{title:'OsmAnd 1.7', url:'blog.html?id=osmand-1-7-released', index: 2},
-	{title:'OsmAnd Seamarks &amp; Bitcoin maps', url:'#', index: 3},
-	{title:'OsmAnd 1.6 Released', url:'#', index: 4},
-	{title:'OsmAnd 1.5 Released', url:'#', index: 5}
+	{title:'OsmAnd Seamarks &amp; Bitcoin maps', url:'blog.html?id=osmand-seamarks-and-bitcoin-maps', index: 3},
+	{title:'OsmAnd 1.6 Released', url:'blog.html?id=osmand-1-6-released', index: 4},
+	{title:'OsmAnd 1.5 Released', url:'blog.html?id=osmand-1-5-released', index: 5}
 ];
 
 
@@ -33,12 +33,6 @@ var init = function(){
 	}
 	var url = 'blog_articles' + '\\' + articleid + ".html";
 	$( ".article" ).load(url);
-
-	/*container.find('a').on('click', function(event){
-		event.preventDefault();
-		var url = 'blog_articles' + '\\' + $.urlParam(this.href, 'id') + ".html";
-		$( ".article" ).load(url);		
-	 });*/
 }
 
 init();
