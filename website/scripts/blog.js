1fbbc42f69bacdad666bc2715d7c4d2310e99cf1
Olga <olga.shcherb@gmail.com>
2015-01-26 23:25:10 +0100

    moved blog articles to another folder

--- website/scripts/blog.js
+++ website/scripts/blog.js
@@ -1,7 +1,7 @@
 var blogArticles = [
-	{title:'OsmAnd 1.9', url:'#', index: 0},
+	{title:'OsmAnd 1.9', url:'blog.html?id=osmand-1-9-released', index: 0},
 	{title:'OsmAnd 1.8', url:'#', index: 1},
-	{title:'OsmAnd 1.7', url:'\blog.html?id=osmand-1-7-released', index: 2},
+	{title:'OsmAnd 1.7', url:'blog.html?id=osmand-1-7-released', index: 2},
 	{title:'OsmAnd Seamarks &amp; Bitcoin maps', url:'#', index: 3},
 	{title:'OsmAnd 1.6 Released', url:'#', index: 4},
 	{title:'OsmAnd 1.5 Released', url:'#', index: 5}
@@ -19,19 +19,26 @@ $.urlParam = function(url, name){
 }
 
 function blog(container){
-
-
+	
 var init = function(){
 	container.empty();
 	
 	for (var link of blogArticles){
 		container.append('<li><a data-index="' + link.index+ '" href="' + link.url + '">' + link.title + '</a></li>');
 	}
-	container.find('a').on('click', function(event){
+	
+	var articleid = $.urlParam(this.href, 'id');
+	if (!articleid || articleid == null){
+		articleid = $.urlParam(blogArticles[0].url, 'id');
+	}
+	var url = 'blog_articles' + '\\' + articleid + ".html";
+	$( ".article" ).load(url);
+
+	/*container.find('a').on('click', function(event){
 		event.preventDefault();
 		var url = 'blog_articles' + '\\' + $.urlParam(this.href, 'id') + ".html";
 		$( ".article" ).load(url);		
-	 });
+	 });*/
 }
 
 init();
