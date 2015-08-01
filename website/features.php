632318e7b4cd2bd8db215bb79d174e3fcd3f97c4
Victor Shcherb <victor.shcherb@gmail.com>
2015-08-01 23:38:14 +0200

    Update features

--- website/features.php
+++ website/features.php
@@ -1,6 +1,6 @@
 <?php
 	if (empty($_GET['id'])) {
-		$_GET['id'] ="online-maps-plugin";
+		$_GET['id'] ="general";
 	}	
 ?>		    	
 <!DOCTYPE html>
@@ -79,13 +79,13 @@
 		  <div class="delimiter"></div>
 		  <ul class="articlelinklist">
 		  	<li><a data-gatag='online-maps-plugin' href="http://osmand.net/features?id=online-maps-plugin">Online maps (Plugin)</a></li>
-		  	<li><a data-gatag='contour-lines-plugin' href="http://osmand.net/features?id=contour-lines-plugin" >Contour lines and hillshade (Plugin)</a></li>
+		  	<li><a data-gatag='contour-lines-plugin' href="http://osmand.net/features?id=contour-lines-plugin" >Contour lines and Hillshade maps(Plugin)</a></li>
 		  	<li><a data-gatag='trip-recording-plugin' href="http://osmand.net/features?id=trip-recording-plugin" >Trip recording (Plugin)</a></li>
 		  	<li><a data-gatag='ski-plugin' href="http://osmand.net/features?id=ski-plugin" >Ski maps (Plugin)</a></li>
 		  	<li><a data-gatag='nautical_charts' href="http://osmand.net/features?id=nautical-charts" >Nautical charts (Plugin)</a></li>
 		  	<li><a data-gatag='audio-video-notes-plugin' href="http://osmand.net/features?id=audio-video-notes-plugin" >Audio/video notes (Plugin)</a></li>
 		  	<li><a data-gatag='osm-editing-plugin' href="http://osmand.net/features?id=osm-editing-plugin">OSM editing (Plugin)</a></li>
-		  	<li><a data-gatag='distance-calculator-and-planning-tool' href="http://osmand.net/features?id=distance-calculator-and-planning-tool">Distance calculator (Plugin)</a></li>
+		  	<li><a data-gatag='distance-calculator-and-planning-tool' href="http://osmand.net/features?id=distance-calculator-and-planning-tool">Distance calculator and planning tool (Plugin)</a></li>
 		  	<li><a data-gatag='parking-plugin' href="http://osmand.net/features?id=parking-plugin" >Parking (Plugin)</a></li>
 		  </ul>
 		</div>
