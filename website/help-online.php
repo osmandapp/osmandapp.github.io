3e609fd69dcd4c7209bd2a85871a37fae34e5ec6
Victor Shcherb <victor.shcherb@gmail.com>
2015-08-24 18:01:47 +0300

    Update help online

--- website/help-online.php
+++ website/help-online.php
@@ -15,15 +15,6 @@
 <script type="text/javascript" src="scripts/jquery-1.11.1.min.js"></script>
 <body>
 
-<!-- for FB-->
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
 
 <div class="maincontainer">
   <div class="main">
@@ -32,20 +23,19 @@
         include 'blocks/simple_header.php';
     ?>
 
-  <?php 
-      include 'blocks/footer.html';
-    ?>
-  </div>
+  
   <div class="articles">
       <div class="articlescontainer">
+        <ul class="articlelist">        
         <li>
         <div class="article">
         <?php
           echo file_get_contents("help/".$_GET['id'].".html");
       ?>
         </div>
-      </li>
-      </div>
+        </li>
+      </ul>
+      
       <div class="acticlestitles">
         <h1>HELP</h1>
         <ul class="articlelinklist">          
@@ -59,7 +49,12 @@
           <li><a data-gatag='technical' href="http://osmand.net/help/Map-Legend_default.html">Techincal articles</a></li>
           <li><a data-gatag='about' href="http://osmand.net/help-online?id=about">About</a></li>
         </ul>
-      </div>
+      </div> <!-- acticlestitles -->
+    </div>  <!-- articlescontainer -->
+  </div> <!-- articles -->
+  <?php 
+      include 'blocks/footer.html';
+    ?>
   </div>
 </div>
 </body>
