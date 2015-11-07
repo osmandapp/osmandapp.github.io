a44190ab502419a56301f5c7f1c8e4f33e603385
Victor Shcherb <victor.shcherb@gmail.com>
2015-11-07 19:06:02 +0100

    update links

--- website/help-online.php
+++ website/help-online.php
@@ -43,13 +43,8 @@
         <ul class="articlelinklist">          
           <li><a data-gatag='faq' href="http://osmand.net/help-online?id=faq">FAQ</a></li>
           <li><a data-gatag='versions' href="http://osmand.net/help-online?id=changes">Versions</a></li>
-          <!-- 
-          <li><a data-gatag='features' href="http://osmand.net/features">Features</a></li>
-          <li><a data-gatag='howto' href="http://osmand.net/help-online?id=HowToArticles">How to articles</a></li>
-        -->
-          <li><a data-gatag='map-legend' href="http://osmand.net/help-online?id=Map-Legend_default">Map legend</a></li>
-          
-          <li><a data-gatag='technical' href="http://osmand.net/help-online?id=TechnicalArticles">Technical articles</a></li>
+          <li><a data-gatag='map-legend' href="http://osmand.net/help-online?id=map-legend">Map legend</a></li>
+          <li><a data-gatag='technical' href="http://osmand.net/help-online?id=technical-articles">Technical articles</a></li>
           <li><a data-gatag='about' href="http://osmand.net/help-online?id=about">About</a></li>
         </ul>
       </div> <!-- acticlestitles -->
