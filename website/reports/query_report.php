f77b60255226982139efbe6ce06566858fcfff26
Victor Shcherb <victor.shcherb@gmail.com>
2016-01-20 21:59:36 +0100

    Cache query report

--- website/reports/query_report.php
+++ website/reports/query_report.php
@@ -1,3 +1,15 @@
-<?php 
-echo  file_get_contents("http://builder.osmand.net/reports/".$_GET["report"].".php?".$_SERVER['QUERY_STRING']);
+<?php
+$memcache = new Memcache;
+$memcache->connect('localhost', 11211) or die ("Can't connect");
+$get_result = $memcache->get($_SERVER['QUERY_STRING']);
+if(!$get_result) {
+  $get_result = file_get_contents("http://builder.osmand.net/reports/".$_GET["report"].".php?".$_SERVER['QUERY_STRING']);
+  $timeout = 120000;
+  if(!isset($_GET['month']) || strlen($_GET["month"]) == 0 || date("Y-m") == $_GET["month"]) {
+  	$timeout = 300; 	
+  }
+  $memcache->set($_SERVER['QUERY_STRING'], $get_result, 0, $timeout);
+
+}
+echo $get_result;
 ?>
