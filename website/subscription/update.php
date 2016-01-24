ab1024d0b6ebd098ca0d626d3fa8c696dc40d2ce
Victor Shcherb <victor.shcherb@gmail.com>
2016-01-24 17:36:43 +0100

    Update register if user exists

--- website/subscription/update.php
+++ website/subscription/update.php
@@ -8,7 +8,7 @@
     $country = pg_escape_string($dbconn, $_GET["preferredCountry"]);
     $userid = pg_escape_string($dbconn, $_GET["userid"]);
   	$result = pg_query($dbconn, "UPDATE supporters SET visiblename='{$visiblename}', useremail='{$useremail}', preferred_region='{$country}' ".
-  		" where userid = '{$userid}' and useremail='${email}';");
+  		" where userid = '{$userid}' and useremail='${email}' RETURNING userid;");
   	if(!$result) {
   		$res = array();        
   		$res['error'] = "Error";
@@ -16,12 +16,18 @@
   		die;
   	}
   	$row = pg_fetch_row($result);
+    if(!$row) {
+      $res = array();        
+      $res['error'] = "Error";
+      echo json_encode($res);
+      die;
+    }
     $res = array();        
     $res['visibleName'] = $_GET["visibleName"]; 
     $res['email'] = $_GET["email"];
     $res['preferredCountry'] = $_GET["preferredCountry"]; 
     $res['userid'] = $_GET["userid"]; 
-	echo json_encode($res);
+	 echo json_encode($res);
   } else {
   	echo file_get_contents("http://builder.osmand.net/subscription/update.php?".$_SERVER['QUERY_STRING']);
   }
