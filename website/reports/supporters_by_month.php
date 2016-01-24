5b36c79bb069c3434e6f559f5e1c662e1cceecf0
Victor Shcherb <victor.shcherb@gmail.com>
2016-01-24 19:15:23 +0100

    Update selection

--- website/reports/supporters_by_month.php
+++ website/reports/supporters_by_month.php
@@ -1,7 +1,4 @@
 <?php
-echo "{'rows':[{'user':'1'}, {'user':'3'}]}";
-die 0;
-
 
 include 'db_conn.php';
 include 'default_vars.php';
@@ -16,15 +13,14 @@ if(!isset($_GET['month'])) {
   $month = $_GET["month"];
 }
 
-$result = pg_query($dbconn, "select ch.userid, ch.visiblename, ch.useremail  
-		from supporters ch where exists ( select 1 from supporters_payments cc where ch.userid = cc.userid 
-		and cc.active = 'true'
-		and substr(cc.payment_period, 0, 8) = '".$month."';");
+$result = pg_query($dbconn, "select ch.userid, ch.visiblename  from supporters ch;");
+#where exists ( select 1 from supporters_payments cc where ch.userid = cc.userid 
+#		and cc.active = 'true'
+#		and substr(cc.payment_period, 0, 8) = '".$month."';
 if (!$result) {
   echo "{'error':'No result'}";
   exit;
 }
-
 
 
 $res = new stdClass();
@@ -37,7 +33,7 @@ while ($row = pg_fetch_row($result)) {
   $email = $row[2];
   if(!$visiblename || strlen($visiblename) == 0) {
   	  $l = strlen($email);
-  	  $visiblename = substr($email, 0, $l / 3 ) . ".." . substr($email, -$l / 3 );
+  	  $visiblename = "USER ".$row[0];
   }
   $rw->user = $visiblename;
 }
