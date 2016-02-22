b0babebe94fd61867d50fee818624dbb712c6d6f
Victor Shcherb <victor.shcherb@gmail.com>
2016-02-22 12:34:11 +0100

    Test db queries

--- website/reports/db_queries.php
+++ website/reports/db_queries.php
@@ -661,7 +661,7 @@ function getAllReports() {
           if($countries->rows[$i]->name == 'World') {
              $iregion = '';
           } else {
-            if($i > 8) {
+            if($i > 15) {
               continue;
             }
             if($countries->rows[$i]->map == '0') {
