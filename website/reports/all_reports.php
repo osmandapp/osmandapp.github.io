a2132041404a579d02affae77b8c8fed161fcf90
Victor Shcherb <victor.shcherb@gmail.com>
2016-02-22 11:51:25 +0100

    Test db queries

--- website/reports/all_reports.php
+++ website/reports/all_reports.php
@@ -4,7 +4,8 @@ set_time_limit(900);
 $r = getAllReports();
 echo "Stored ".count($r->reports)." reports\n";
 if(isset($_REQUEST['eurValue'])) {
-	echo "Total payouts ".json_encode($r->payoutTotal)."\n";
+	//echo "Total payouts ".json_encode($r->payoutTotal)."\n";
+	echo "Total payouts ".json_encode($r)."\n";
 	echo "Payouts ".json_encode($r->payouts)."\n";
 }
 ?>
