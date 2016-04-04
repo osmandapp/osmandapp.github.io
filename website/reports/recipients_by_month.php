86c1b33def84f1141a8ec83c7c3b07852fabac2d
vshcherb <victor.shcherb@gmail.com>
2016-04-04 14:01:26 +0300

    Update recipients_by_month.php

--- website/reports/recipients_by_month.php
+++ website/reports/recipients_by_month.php
@@ -39,9 +39,9 @@ if(is_null(getReport('getBTCValue'))) {
 		$res->message = $res->message . 'Payouts: <a href="https://blockchain.info/tx/8158da594891265e36ec2ba531061e7edcde27e2a46b459c9019bfa280b2cf85">Transaction #1</a>';
 	} else if($res->month == '2016-03') {
 		$res->message = $res->message . 'Payouts: <a href="https://blockchain.info/tx/3972ae6a30ed4a97f3b448f5bef360e6ca9858dec4e2c1fce4d5971b0897823e">Transaction #1</a>';
-		$res->message = $res->message . '<a href="https://blockchain.info/tx/3f1fc2d9bd0abc184d77d5457c915ed5252bce21dfda0299347fee863423ee73">Transaction #2</a>';
-		$res->message = $res->message . '<a href="https://blockchain.info/tx/b86aa1773e2c256da33632c725b1d5c5227aad406bff5fbf27924c30298ec426">Transaction #3</a>';
-		$res->message = $res->message . '<a href="https://blockchain.info/tx/513f7ada01e01cbb56f87ea8d70c8da4e3b8c9e49e19c44813f7a56669b499d3">Transaction #4</a>';
+		$res->message = $res->message . ',<nbsp><a href="https://blockchain.info/tx/3f1fc2d9bd0abc184d77d5457c915ed5252bce21dfda0299347fee863423ee73">Transaction #2</a>';
+		$res->message = $res->message . ',<nbsp><a href="https://blockchain.info/tx/b86aa1773e2c256da33632c725b1d5c5227aad406bff5fbf27924c30298ec426">Transaction #3</a>';
+		$res->message = $res->message . ',<nbsp><a href="https://blockchain.info/tx/513f7ada01e01cbb56f87ea8d70c8da4e3b8c9e49e19c44813f7a56669b499d3">Transaction #4</a>';
 		
 	}
 }
