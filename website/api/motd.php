b1299e4c9279533e6015eaaff02dd795be7b0b56
vshcherb <victor.shcherb@gmail.com>
2017-02-08 10:11:51 +0100

    Update motd.php

--- website/api/motd.php
+++ website/api/motd.php
@@ -1,3 +1,14 @@
 <?php
- echo file_get_contents("./discount.json");
+if(isset($_GET['os']) && $_GET['os'] == 'ios') {
+  // url supported: 
+  // 1. screen in app [screen:in_apps], [in_app:<inappid>]
+  // 2. http url supported
+  echo file_get_contents("./discount_ios.json");
+} else {
+ // url supported: 
+ // 1. "osmand-market-app:net.osmand.plus" - market redirect
+ // 2. http url supported
+  echo file_get_contents("./discount.json");
+}
+
 ?>
