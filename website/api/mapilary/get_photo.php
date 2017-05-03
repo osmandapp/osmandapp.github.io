184fe3386d9fab51c236dfc9856d11e97493fe6e
Alexey <alexey.kulish@osmand.net>
2017-05-04 01:25:17 +0300

    Update get_photo.php

--- website/api/mapilary/get_photo.php
+++ website/api/mapilary/get_photo.php
@@ -1,4 +1,4 @@
 <?php
  header("Content-type: image/jpeg");
- echo file_get_contents("https://d1cuyjsrcm0gby.cloudfront.net/".$_GET['photoid']."/thumb-640.jpg?origin=osmand");
+ echo file_get_contents("https://d1cuyjsrcm0gby.cloudfront.net/".$_GET['photo_id']."/thumb-640.jpg?origin=osmand");
 ?>
