8bbe67a7567a0d548fa2a84c00dddda07c56c2c0
Victor Shcherb <victor.shcherb@gmail.com>
2017-05-03 11:21:52 +0200

    Add api

--- website/api/cm_place.php
+++ website/api/cm_place.php
@@ -1,5 +1,5 @@
 <?php
 
- $txt = $_GET['lat'].','.$_GET['lon'];
- echo file_get_contents("https://a.mapillary.com/v3/images/?lookat=".$txt."&closeto=".$txt."&radius=50&client_id=LXJVNHlDOGdMSVgxZG5mVzlHQ3ZqQTo0NjE5OWRiN2EzNTFkNDg4");
+ $txt = $_GET['lon'].','.$_GET['lat'];
+ echo file_get_contents("https://a.mapillary.com/v3/images/?closeto=".$txt."&radius=50&client_id=LXJVNHlDOGdMSVgxZG5mVzlHQ3ZqQTo0NjE5OWRiN2EzNTFkNDg4");
 ?>
