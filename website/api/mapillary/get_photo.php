b655acf3095394d7bcf79c72789c1d801c5b586a
Alexey <alexey.kulish@osmand.net>
2017-05-08 19:43:29 +0300

    Added hires param

--- website/api/mapillary/get_photo.php
+++ website/api/mapillary/get_photo.php
@@ -1,4 +1,8 @@
 <?php
  header("Content-type: image/jpeg");
- echo file_get_contents("https://d1cuyjsrcm0gby.cloudfront.net/".$_GET['photo_id']."/thumb-640.jpg?origin=osmand");
+ if(isset($_GET['hires'])) {
+   echo file_get_contents("https://d1cuyjsrcm0gby.cloudfront.net/".$_GET['photo_id']."/thumb-1024.jpg?origin=osmand");
+ } else {
+   echo file_get_contents("https://d1cuyjsrcm0gby.cloudfront.net/".$_GET['photo_id']."/thumb-640.jpg?origin=osmand");
+ }
 ?>
