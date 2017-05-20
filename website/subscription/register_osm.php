8eaf5cb4b824d21a5872a9be974cf001b9137b7f
vshcherb <victor.shcherb@gmail.com>
2017-05-20 15:24:20 +0200

    Update register_osm.php

--- website/subscription/register_osm.php
+++ website/subscription/register_osm.php
@@ -52,8 +52,9 @@
 
     $osm_usr = pg_escape_string($dbconn, $_POST["osm_usr"]);
     $osm_pwd = pg_escape_string($dbconn, $_POST["osm_pwd"]);
+    $email = pg_escape_string($dbconn, $_POST["email"]);	  
+	  
     $bitcoin = pg_escape_string($dbconn, $_POST["bitcoin_addr"]);
-    // $email = pg_escape_string($dbconn, $_POST["email"]);
     $email = "";
     $time = time() * 1000;
     $basic = base64_encode($osm_usr.":".$osm_pwd);
