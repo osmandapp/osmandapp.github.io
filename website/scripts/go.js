f5579995a843cb25050b66127e955d89cd71f45c
Olga <olga.shcherb@gmail.com>
2017-06-02 23:33:45 +0200

    Refactored javascript, aligned coordinates

--- website/scripts/go.js
+++ website/scripts/go.js
@@ -1,51 +1,167 @@
-
-function gomap(mapcontainer, pagecontainer, footer, latitudeElement, longitudeElement){
-var map =null;
-
-var getParamValue = function(paramName){
-     return (location.search.split(paramName + '=')[1]||'').split('&')[0];
+var requestUtils={
+	'getParamValue':function(paramName){		
+			let value= (location.search.split(paramName + '=')[1]||'').split('&')[0];
+			if (value && value.length > 0){
+				return value;
+			}
+			return null;
+		},
+	'isIOS':function(){
+		return /(iPad|iPhone|iPod)/g.test( navigator.userAgent );
+	},
+	'redirect':function(newUrl){
+		document.location = newUrl;
+	}
 };
-var init = function(){
-   /*$('#map').height($(document).height()-233);
-	var newWidth = $(document).width();	
-	$('#map').width(newWidth);*/
-	var lat = getParamValue('lat');
-	var pointDefined = true;
-	if (!lat){
-		lat = 51.505;
-		pointDefined = false;
-	}
-	var lon = getParamValue('lon');
-	if (!lon){
-		lon = -0.09;
-		pointDefined = false;
-	}
-	var z = getParamValue('z');
-	if (!z){
-		z = 13;
-		pointDefined = false;
-	}
-	var container = $(".gocontainer");
-	
-	var footer = $(".gofooter");
-	footer.remove();
 
-	map = L.map('map').setView([lat, lon], z);
-	L.tileLayer('http://tile.osmand.net/hd/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-		maxZoom: 20,
-    		maxNativeZoom: 19
-	}).addTo(map);
-	
-	
-	container.append(footer);
-	if (pointDefined == true){
-		var marker = L.marker([lat, lon]).addTo(map);
+var goMap = {
+	'config':{
+		'containerid': 'gocontainer',		
+		'defaults':{
+			'lat':51.505,
+			'lon':-0.09,
+			'zoom':13
+			}		
+	},
+	'utils':{
+		'getPointFromUrl':function(){
+			let point = {};
+			point.lat = requestUtils.getParamValue('lat');
+			point.lon = requestUtils.getParamValue('lon');
+			point.zoom = requestUtils.getParamValue('z');
+			return point;
+		},
+		'isPointComplete':function(point){		
+			if (!point.lat || !point.lon){
+				return false;
+			}
+			return true;
+		},
+		'extendPoint':function(initialPoint, newPoint){
+			let point={};
+			point.lat=newPoint.lat;
+			if (!point.lat || point.lat == null){
+				point.lat = initialPoint.lat;
+			}
+			point.lon=newPoint.lon;
+			if (!point.lon || point.lon == null){
+				point.lon = initialPoint.lon;
+			}
+			point.zoom=newPoint.zoom;
+			if (!point.zoom || point.zoom == null){
+				point.zoom = initialPoint.zoom;
+			}
+			return point;
+		}
+	},
+	'init': function(config){
+		if (config && typeof (config) == 'object') {
+            $.extend(goMap.config, config);
+        }
+		goMap.$container = $('#' + goMap.config.containerid);
+		goMap.$footer = goMap.$container.find('.gofooter');
+		goMap.$latitude = goMap.$container.find('.latitude');
+		goMap.$longitude = goMap.$container.find('.longitude');
+		goMap.applestorelink = goMap.$container.find('.gobadges .apple a').attr('href');
 		
+		let inputPoint = goMap.utils.getPointFromUrl();					
+		goMap.point = goMap.utils.extendPoint(goMap.config.defaults, inputPoint);
+		goMap.refreshCoordinates();
+		
+		goMap.map =$.mapwidget();		
+		goMap.map.showPoint(goMap.point);
+		
+		let inputComplete = goMap.utils.isPointComplete(inputPoint);
+		if (inputComplete){
+			goMap.map.addMarker(goMap.point);
+		}
+		goMap.point = goMap.utils.getPointFromUrl();	
+
+		if (requestUtils.isIOS()){
+			goMap.timer = $.timer({action:requestUtils.redirect, actionparams:goMap.applestorelink});
+			goMap.timer.start();
+		}
+	},	
+	'refreshCoordinates':function(){
+		goMap.$latitude.text(goMap.point.lat);
+		goMap.$longitude.text(goMap.point.lon);
 	}
-	$(".latitude").text(lat);
-	$(".longitude").text(lon);
 };
 
-init();
-}
+(function($) {
+	$.mapwidget = function(config) {
+		var mapobj={
+			config: $.extend({
+				'mapid':'map',
+                'maxzoom':20,
+				'maxnativezoom':19,
+				'sourceurl':'http://tile.osmand.net/hd/{z}/{x}/{y}.png',
+				'attribution':'&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+            }, config),
+			init:function(){
+				mapobj.map = L.map(mapobj.config.mapid);
+				L.tileLayer(mapobj.config.sourceurl, {
+					attribution: mapobj.config.attribution,
+					maxZoom: mapobj.config.maxzoom,
+					maxNativeZoom: mapobj.config.maxnativezoom
+				}).addTo(mapobj.map);				
+			},
+			showPoint:function(point){
+				mapobj.map.setView([point.lat, point.lon], point.zoom);				
+			},
+			addMarker:function(point){
+				L.marker([point.lat, point.lon]).addTo(mapobj.map);
+			}
+		};
+		mapobj.init();
+		return {            
+            showPoint: mapobj.showPoint,
+            addMarker: mapobj.addMarker
+        };
+	};
+})(jQuery);
+
+(function($) {
+	$.timer=function(config){
+		var timerobj={
+			config: $.extend({
+				'timeoutInMs':300,
+                'maxActionDelayInMs':2000,
+				'action':function(){},
+				'actionparams':null
+            }, config),
+			init:function(){
+				timerobj.timer = null;
+				timerobj.startDate = null;
+			},
+			start:function(){
+				timerobj.cancel();
+				timerobj.startDate = new Date();
+				timerobj.timer=setTimeout(timerobj.onTimer, timerobj.config.timeoutInMs);
+			},
+			cancel:function(){
+				if (timerobj.timer != null){
+					clearTimeout(timerobj.timer);
+					timerobj.timer = null;
+					timerobj.startDate = null;
+				}
+			},
+			onTimer:function(){
+				timerobj.timer= null;
+				let now = new Date();
+				if(now - timerobj.startDate < timerobj.config.maxActionDelayInMs){
+					timerobj.config.action(timerobj.config.actionparams);
+				}				
+			}
+		};
+		timerobj.init();
+		return {
+			start:timerobj.start,
+			cancel:timerobj.cancel
+		};
+	};
+})(jQuery);
+
+ $( document ).ready(function() {
+    goMap.init();
+  });
