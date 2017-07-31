a4ab929f0335f5b2c889b003aae726a8061efcb8
vshcherb <victor.shcherb@gmail.com>
2017-07-31 12:59:48 +0300

    Test get photo

--- website/api/mapillary/get_photo.php
+++ website/api/mapillary/get_photo.php
@@ -1,8 +1,12 @@
 <?php
  header("Content-type: image/jpeg");
+ header('HTTP/1.1 302 Found');
+
  if(isset($_GET['hires'])) {
-   echo file_get_contents("https://d1cuyjsrcm0gby.cloudfront.net/".$_GET['photo_id']."/thumb-1024.jpg?origin=osmand");
+//   echo file_get_contents("https://d1cuyjsrcm0gby.cloudfront.net/".$_GET['photo_id']."/thumb-1024.jpg?origin=osmand");
+    header("Location: https://d1cuyjsrcm0gby.cloudfront.net/".$_GET['photo_id']."/thumb-1024.jpg?origin=osmand");
  } else {
-   echo file_get_contents("https://d1cuyjsrcm0gby.cloudfront.net/".$_GET['photo_id']."/thumb-640.jpg?origin=osmand");
+//   echo file_get_contents("https://d1cuyjsrcm0gby.cloudfront.net/".$_GET['photo_id']."/thumb-640.jpg?origin=osmand");
+    header("Location: https://d1cuyjsrcm0gby.cloudfront.net/".$_GET['photo_id']."/thumb-640.jpg?origin=osmand");
  }
 ?>
