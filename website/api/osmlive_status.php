f6345064c67fb043260a0fdad2541c850ab67b7e
PaulStets <stetsenko.pavlo@gmail.com>
2017-09-07 12:57:41 +0300

    Update osmlive_status.php

--- website/api/osmlive_status.php
+++ website/api/osmlive_status.php
@@ -1,25 +1,20 @@
 <?php
-$path = "/home/osm-planet/osmlive/_diff"; 
 
+$path = "/home/paul/osmand/"; 
+
+ 
 $latest_ctime = 0;
+   
 
-/*$d = dir($path);
+$d = dir($path);
 while (false !== ($entry = $d->read())) {
   $filepath = "{$path}/{$entry}";
   // could do also other checks than just checking whether the entry is a file
   if (is_file($filepath) && filemtime($filepath) > $latest_ctime) {
     $latest_ctime = filemtime($filepath);
   }
-}*/
-
-if ($handle = opendir($path)) {
-    while (false !== ($file = readdir($handle))) {
-        if (is_file($filepath) && filemtime($filepath) > $latest_ctime) {
-            $latest_ctime = filemtime($filepath);
-        }
-        
-    }
-    closedir($handle);
+}
 
 echo date('Y-m-d H:i:s', $latest_ctime);
+
 ?>
