1a17782c9caad108efdb1f808105fa8b794e997d
PaulStets <stetsenko.pavlo@gmail.com>
2017-09-07 14:50:57 +0300

    Update osmlive_status.php

--- website/api/osmlive_status.php
+++ website/api/osmlive_status.php
@@ -9,10 +9,9 @@ $files = scandir($path);
 foreach($files as $file) 
 {
   $fullpath = "{$path}/{$file}";
-  /*if (is_file($fullpath) && filemtime($fullpath) > $latest_ctime) {
+  if (is_file($fullpath) && filemtime($fullpath) > $latest_ctime) {
     $latest_ctime = filemtime($fullpath);
-  }*/
-  
+  }
 }
 echo date('Y-m-d H:i:s', $latest_ctime);
 
