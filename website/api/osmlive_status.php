67f9f70e770a010fbbd658bdfad56065738e5764
PaulStets <stetsenko.pavlo@gmail.com>
2017-09-07 18:50:05 +0300

    Update osmlive_status.php

--- website/api/osmlive_status.php
+++ website/api/osmlive_status.php
@@ -1,16 +1,17 @@
 <?php
 error_reporting(E_ALL); 
 ini_set('display_errors','1');
-$file = fopen(__DIR__."/../.proc_timestamp","r");
+$path = "/var/www-download/_diff";
+/*$file = fopen(__DIR__."/../.proc_timestamp","r");
 if (!$file) {
     echo 'Could not open file';
 }
 $date = fgets($file);
 echo $date;
 echo "test";
-fclose($file);
+fclose($file);*/
 
-/*$latest_ctime = 0;
+$latest_ctime = 0;
 if (is_link($path)) {
   echo "is symlink";
 }
@@ -24,5 +25,5 @@ foreach($files as $file)
     $latest_ctime = filemtime($fullpath);
   }
 }
-echo date('Y-m-d H:i:s', $latest_ctime);*/
+echo date('Y-m-d H:i:s', $latest_ctime);
 ?>
