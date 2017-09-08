5122a809203c1401ddbb768499d942cf8a34651b
PaulStets <stetsenko.pavlo@gmail.com>
2017-09-08 12:05:32 +0300

    Update osmlive_status.php

--- website/api/osmlive_status.php
+++ website/api/osmlive_status.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL); 
 ini_set('display_errors','1');
 $path = "/var/www-download/_diff";
-$file = fopen(__DIR__."/../.proc_timestamp","r");
+$file = fopen("../.proc_timestamp","r");
 $date = fgets($file);
 echo $date;
 fclose($file);
