066b615f1989cf1165ebb8b85dc0837fe7e113e4
Victor Shcherb <victor.shcherb@gmail.com>
2017-09-08 13:07:37 +0300

    Fix osmlive status

--- website/api/osmlive_status.php
+++ website/api/osmlive_status.php
@@ -1,28 +1,3 @@
 <?php
-error_reporting(E_ALL); 
-ini_set('display_errors','1');
-$path = "/var/www-download/";
-print_r(scandir ($path));
-$file = fopen(__DIR__."/../proc_timestamp","r");
-$date = fgets($file);
-echo $date;
-fclose($file);
-
-/*
-$latest_ctime = 0;
-if (is_link($path)) {
-  echo "is symlink";
-}
-
-$files = scandir($path);
-/*
-foreach($files as $file) 
-{
-  $fullpath = "{$path}/{$file}";
-  if (is_file($fullpath) && filemtime($fullpath) > $latest_ctime) {
-    $latest_ctime = filemtime($fullpath);
-  }
-}
-echo date('Y-m-d H:i:s', $latest_ctime);*/
-
+	echo file_get_contents(".proc_timestamp");
 ?>
