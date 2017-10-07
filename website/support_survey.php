a568c0791e21db9f8cbfdc5f5bec839bc722f528
PaulStets <stetsenko.pavlo@gmail.com>
2017-10-07 18:01:04 +0300

    Update support_survey.php

--- website/support_survey.php
+++ website/support_survey.php
@@ -5,6 +5,9 @@ require "./vendor/autoload.php';
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\ServiceAccount;
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/google-service-account.json');
 $apiKey = 'AIzaSyCYcJNbJ4X6irZtS37VYtNJHMxv_nCf0vw';
 
