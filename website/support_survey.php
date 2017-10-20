e5f726a8dfbfca189fbc1c93c86f9110be87f2f0
PaulStets <stetsenko.pavlo@gmail.com>
2017-10-20 12:56:26 +0300

    Update support_survey.php

--- website/support_survey.php
+++ website/support_survey.php
@@ -49,6 +49,18 @@ elseif ($_GET["response"] == 'bad') {
     include 'blocks/default_links.html';
   ?>
   <title>OsmAnd Survey</title>
+  <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
 </head>
 <body>
   <style>
@@ -86,5 +98,13 @@ elseif ($_GET["response"] == 'bad') {
       </div>
       <img src="./images/logo-grey.png" alt="Logo" style="width:150px;height:50%;margin-top:20px">
   </div>
+  <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
 </body>
 </html>
