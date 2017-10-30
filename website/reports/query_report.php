ef64f3c896704df3a1f54e535144e81292c4b156
Victor Shcherb <victor.shcherb@gmail.com>
2017-10-30 20:51:06 +0100

    Update server site for osmlive reports

--- website/reports/query_report.php
+++ website/reports/query_report.php
@@ -7,14 +7,23 @@ if($_SERVER['SERVER_NAME'] == 'builder.osmand.net') {
 	$key_mem = str_replace("&force=true","",$key_mem);
 	$get_result = $memcache->get($key_mem);
 	$timeout = 24*60*60; 
+	$currentTime = time();
 	if(!$get_result || isset($_REQUEST['force']) ) {
+		if ($get_result) {
+			$json_prev = json_decode($get_result); 
+		}
   		$get_result = file_get_contents("http://builder.osmand.net/reports/".$_GET["report"].".php?".$_SERVER['QUERY_STRING']);
   		$json_res = json_decode($get_result);
-  		$json_res->timeout = time();
+  		$json_res->generationTime = $currentTime;
+  		$nextTimeout = $timeout;
+  		if($json_prev && $json_prev->expirationTime) {
+  			$nextTimeout = $json_prev->expirationTime - $currentTime;
+  		}
   		//if($_GET["report"] == "supporters_by_month") {
   		//	$timeout = 10;
   		//}
-  		$memcache->set($key_mem, json_encode($json_res), $timeout);
+  		$json_res->expirationTime = $currentTime + $nextTimeout;
+  		$memcache->set($key_mem, json_encode($json_res), $nextTimeout);
   		$get_result = json_encode($json_res);
 	} else {
 		// keep 1 day at least
