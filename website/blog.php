da45846d6e8db34018b85d369401785f12ad322d
Victor Shcherb <victor.shcherb@gmail.com>
2017-10-30 19:02:19 +0100

    Update blog links

--- website/blog.php
+++ website/blog.php
@@ -61,37 +61,37 @@
           <div class="acticlestitles">
             <h2>Latest articles</h2>
             <ul class="articlelinklist">
-              <li><a data-gatag='osmand_youtube_channel' href="http://osmand.net/blog?id=osmand-2-8-released" >OsmAnd 2.8</a></li>
-              <li><a data-gatag='osmand_youtube_channel' href="http://osmand.net/blog?id=osmand-2-7-released" >OsmAnd 2.7</a></li>
-              <li><a data-gatag='osmand_youtube_channel' href="http://osmand.net/blog?id=osmand-2-6-released" >OsmAnd 2.6</a></li>
-              <li><a data-gatag='osmand_youtube_channel' href="http://osmand.net/blog?id=osmand_youtube_channel" >OsmAnd team on Youtube</a></li>
-              <li><a data-gatag='osmand-ios-1.3-released' href="http://osmand.net/blog?id=osmand-ios-1.3-released" >OsmAnd 1.3 (iOS)</a></li>
-              <li><a data-gatag='osmand-2-5-released' href="http://osmand.net/blog?id=osmand-2-5-released" >OsmAnd 2.5</a></li>
-              <li><a data-gatag='christmas_map' href="http://osmand.net/blog?id=christmas_map" >Сhristmas POI map</a></li>
-              <li><a data-gatag='topo-style' href="http://osmand.net/blog?id=topo_style" >Topo style</a></li>
-              <li><a data-gatag='osmand-2-4-released' href="http://osmand.net/blog?id=osmand-2-4-released" >OsmAnd 2.4</a></li>
-              <li><a data-gatag='osmand_videos' href="http://osmand.net/blog?id=osmand_videos" >OsmAnd Videos</a></li>
-              <li><a data-gatag='api_demo' href="http://osmand.net/blog?id=api_demo" >OsmAnd API Demo</a></li>
-              <li><a data-gatag='osm-live' href="http://osmand.net/blog?id=osm-live" >OSM Live</a></li>
-              <li><a data-gatag='osmand-2-3-released' href="http://osmand.net/blog?id=osmand-2-3-released" >OsmAnd 2.3</a></li>
-              <li><a data-gatag='osmand_google_comparison' href="http://osmand.net/blog?id=osmand_google_comparison" >Google maps vs. OsmAnd maps  & navigation</a></li>
-              <li><a data-gatag='osmand-2-2-released' href="http://osmand.net/blog?id=osmand-2-2-released" >OsmAnd 2.2</a></li>
-              <li><a data-gatag='osmand-ios-1.2.2-released' href="http://osmand.net/blog?id=osmand-ios-1.2.2-released" >OsmAnd 1.2.2 (iOS)</a></li>
-              <li><a data-gatag='osmand-ios-1.1.1-released' href="http://osmand.net/blog?id=osmand-ios-1.1.1-released" >OsmAnd 1.1.1 (iOS)</a></li>
-              <li><a data-gatag='osmand-5-years' href="http://osmand.net/blog?id=5_years" >5 years!</a></li>
-              <li><a data-gatag='osmand-2-1-released' href="http://osmand.net/blog?id=osmand-2-1-released" >OsmAnd 2.1</a></li>
-              <li><a data-gatag='osmand_ios_1_0_2' href="http://osmand.net/blog?id=osmand-ios-1.0.2" >OsmAnd 1.0.2 (iOS)</a></li>
-              <li><a data-gatag='osmand_2_0' href="http://osmand.net/blog?id=osmand-2-0-released" >OsmAnd 2.0</a></li>
-              <li><a data-gatag='osmand_ios' href="http://osmand.net/blog?id=osmand-ios" >OsmAnd for iPhone is released</a></li>
-              <li><a data-gatag='nautical_charts' href="http://osmand.net/blog?id=nautical-charts" >Nautical charts</a></li>
-              <li><a data-gatag='osmand_dvr_goes_live' href="http://osmand.net/blog?id=osmand-dvr-goes-live">OsmAnd DVR goes live</a></li>
-              <li><a data-gatag='osmand_1_9' href="http://osmand.net/blog?id=osmand-1-9-released" >OsmAnd 1.9</a></li>
-              <li><a data-gatag='osmand_1_8' href="http://osmand.net/blog?id=osmand-1-8-released" >OsmAnd 1.8</a></li>
-              <li><a data-gatag='osmand_1_7' href="http://osmand.net/blog?id=osmand-1-7-released" >OsmAnd 1.7</a></li>
-              <li><a data-gatag='osmand_1_6' href="http://osmand.net/blog?id=osmand-1-6-released" >OsmAnd 1.6</a></li>
-              <li><a data-gatag='osmand_1_5' href="http://osmand.net/blog?id=osmand-1-5-released" >OsmAnd 1.5</a></li>
-              <li><a data-gatag='osmand_videos' href="http://osmand.net/blog?id=osmand_videos" >OsmAnd Videos</a></li>
-              <li><a data-gatag='osmand_rss' href="http://osmand.net/rss.xml" >RSS</a></li>
+              <li><a data-gatag='osmand-2-8-released' href="/blog?id=osmand-2-8-released" >OsmAnd 2.8</a></li>
+              <li><a data-gatag='osmand-2-7-released' href="/blog?id=osmand-2-7-released" >OsmAnd 2.7</a></li>
+              <li><a data-gatag='osmand-2-6-released' href="/blog?id=osmand-2-6-released" >OsmAnd 2.6</a></li>
+              <li><a data-gatag='osmand_youtube_channel' href="/blog?id=osmand_youtube_channel" >OsmAnd team on Youtube</a></li>
+              <li><a data-gatag='osmand-ios-1.3-released' href="/blog?id=osmand-ios-1.3-released" >OsmAnd 1.3 (iOS)</a></li>
+              <li><a data-gatag='osmand-2-5-released' href="/blog?id=osmand-2-5-released" >OsmAnd 2.5</a></li>
+              <li><a data-gatag='christmas_map' href="/blog?id=christmas_map" >Сhristmas POI map</a></li>
+              <li><a data-gatag='topo-style' href="/blog?id=topo_style" >Topo style</a></li>
+              <li><a data-gatag='osmand-2-4-released' href="/blog?id=osmand-2-4-released" >OsmAnd 2.4</a></li>
+              <li><a data-gatag='osmand_videos' href="/blog?id=osmand_videos" >OsmAnd Videos</a></li>
+              <li><a data-gatag='api_demo' href="/blog?id=api_demo" >OsmAnd API Demo</a></li>
+              <li><a data-gatag='osm-live' href="/blog?id=osm-live" >OSM Live</a></li>
+              <li><a data-gatag='osmand-2-3-released' href="/blog?id=osmand-2-3-released" >OsmAnd 2.3</a></li>
+              <li><a data-gatag='osmand_google_comparison' href="/blog?id=osmand_google_comparison" >Google maps vs. OsmAnd maps  & navigation</a></li>
+              <li><a data-gatag='osmand-2-2-released' href="/blog?id=osmand-2-2-released" >OsmAnd 2.2</a></li>
+              <li><a data-gatag='osmand-ios-1.2.2-released' href="/blog?id=osmand-ios-1.2.2-released" >OsmAnd 1.2.2 (iOS)</a></li>
+              <li><a data-gatag='osmand-ios-1.1.1-released' href="/blog?id=osmand-ios-1.1.1-released" >OsmAnd 1.1.1 (iOS)</a></li>
+              <li><a data-gatag='osmand-5-years' href="/blog?id=5_years" >5 years!</a></li>
+              <li><a data-gatag='osmand-2-1-released' href="/blog?id=osmand-2-1-released" >OsmAnd 2.1</a></li>
+              <li><a data-gatag='osmand_ios_1_0_2' href="/blog?id=osmand-ios-1.0.2" >OsmAnd 1.0.2 (iOS)</a></li>
+              <li><a data-gatag='osmand_2_0' href="/blog?id=osmand-2-0-released" >OsmAnd 2.0</a></li>
+              <li><a data-gatag='osmand_ios' href="/blog?id=osmand-ios" >OsmAnd for iPhone is released</a></li>
+              <li><a data-gatag='nautical_charts' href="/blog?id=nautical-charts" >Nautical charts</a></li>
+              <li><a data-gatag='osmand_dvr_goes_live' href="/blog?id=osmand-dvr-goes-live">OsmAnd DVR goes live</a></li>
+              <li><a data-gatag='osmand_1_9' href="/blog?id=osmand-1-9-released" >OsmAnd 1.9</a></li>
+              <li><a data-gatag='osmand_1_8' href="/blog?id=osmand-1-8-released" >OsmAnd 1.8</a></li>
+              <li><a data-gatag='osmand_1_7' href="/blog?id=osmand-1-7-released" >OsmAnd 1.7</a></li>
+              <li><a data-gatag='osmand_1_6' href="/blog?id=osmand-1-6-released" >OsmAnd 1.6</a></li>
+              <li><a data-gatag='osmand_1_5' href="/blog?id=osmand-1-5-released" >OsmAnd 1.5</a></li>
+              <li><a data-gatag='osmand_videos' href="/blog?id=osmand_videos" >OsmAnd Videos</a></li>
+              <li><a data-gatag='osmand_rss' href="/rss.xml" >RSS</a></li>
             </ul>
           </div>
         </div>
