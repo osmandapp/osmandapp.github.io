94a65947c015e085b325c28114db4af0c0b34fa9
Victor Shcherb <victor.shcherb@gmail.com>
2017-11-01 00:56:21 +0100

    Be more verbose in printing reports

--- website/reports/all_reports.php
+++ website/reports/all_reports.php
@@ -1,4 +1,12 @@
 <?php
+if(isset($argv[1]) {
+   $_REQUEST['month'] = $argv[1];
+   $_REQUEST['dbmonth'] = $argv[1];
+   if(isset($argv[2]) {
+        $_REQUEST['eurValue'] = $argv[2];
+        $_REQUEST['btcValue'] = $argv[3];
+   }
+}
 include 'db_queries.php';
 set_time_limit(900);
 $r = getAllReports();
