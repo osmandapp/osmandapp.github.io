b48c1598ca28ae813ebd9569411dd4824c11c115
Victor Shcherb <victor.shcherb@gmail.com>
2017-11-01 11:47:23 +0100

    Fix all reports

--- website/reports/all_reports.php
+++ website/reports/all_reports.php
@@ -2,10 +2,13 @@
 if(isset($argv[1])) {
    $_REQUEST['month'] = $argv[1];
    $_REQUEST['dbmonth'] = $argv[1];
-   $_REQUEST['dbmonth'] = $argv[1];
+   $_GET['month'] = $argv[1];
+   $_GET['dbmonth'] = $argv[1];
    if(isset($argv[2])) {
         $_REQUEST['eurValue'] = $argv[2];
         $_REQUEST['btcValue'] = $argv[3];
+        $_GET['eurValue'] = $argv[2];
+        $_GET['btcValue'] = $argv[3];
    }
    $_SERVER['REQUEST_METHOD'] = 'POST';
 }
