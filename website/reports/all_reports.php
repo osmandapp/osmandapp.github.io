61b61fc48d6c2a2bca29dfa2c0f3ea208705679d
Victor Shcherb <victor.shcherb@gmail.com>
2017-11-01 02:10:11 +0100

    Fix all reports

--- website/reports/all_reports.php
+++ website/reports/all_reports.php
@@ -2,13 +2,16 @@
 if(isset($argv[1])) {
    $_REQUEST['month'] = $argv[1];
    $_REQUEST['dbmonth'] = $argv[1];
+   $_REQUEST['dbmonth'] = $argv[1];
    if(isset($argv[2])) {
         $_REQUEST['eurValue'] = $argv[2];
         $_REQUEST['btcValue'] = $argv[3];
    }
+   $_SERVER['REQUEST_METHOD'] = 'POST';
 }
 include 'db_queries.php';
 set_time_limit(900);
+echo "Generate all reports\n";
 $r = getAllReports();
 echo "Stored ".count($r->reports)." reports\n";
 if(isset($_REQUEST['eurValue'])) {
