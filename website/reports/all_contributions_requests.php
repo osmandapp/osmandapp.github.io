1e7d6ba99118d480fdeb69c1ebab96e45b638666
PaulStets <stetsenko.pavlo@gmail.com>
2017-11-04 18:29:36 +0200

    Test file for unufying the requests

--- website/reports/all_contributions_requests.php
+++ website/reports/all_contributions_requests.php
@@ -1,7 +1,7 @@
 <?php
 include_once 'db_queries.php';
-$data = calculateUserRanking();
-$firstMerge = array_merge(calculateUserRanking(), calculateRanking());
-$secondMerge = array_merge($firstMerge, getTotalChanges());
+$data = (array) calculateUserRanking();
+$firstMerge = array_merge($data, (array) calculateRanking());
+$secondMerge = array_merge($firstMerge, (array) getTotalChanges());
 echo json_encode($secondMerge);
 ?>
