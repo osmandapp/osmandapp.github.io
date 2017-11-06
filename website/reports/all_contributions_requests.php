6fcd4bb2e2c334a559a7bf226ac9653afa4c949b
PaulStets <stetsenko.pavlo@gmail.com>
2017-11-06 15:53:19 +0200

    Test query optimization

--- website/reports/all_contributions_requests.php
+++ website/reports/all_contributions_requests.php
@@ -3,7 +3,7 @@ include_once 'db_queries.php';
 //$data = (array) calculateUserRanking();
 //$firstMerge = array_merge($data, (array) calculateRanking());
 //$secondMerge = array_merge($firstMerge, (array) getTotalChanges());
-echo json_encode(getTotalChanges());
-echo json_encode(calculateUserRanking());
+echo json_encode(getTotalChanges()) + "\n";
+echo json_encode(calculateUserRanking()) + "\n";
 echo json_encode(calculateRanking());
 ?>
