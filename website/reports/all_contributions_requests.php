96ba4d6508c1b35a66d9d896c5adf6ee01f3097c
PaulStets <stetsenko.pavlo@gmail.com>
2017-11-07 12:27:28 +0200

    Removed unused queries and added date

--- website/reports/all_contributions_requests.php
+++ website/reports/all_contributions_requests.php
@@ -1,8 +1,5 @@
 <?php
 include_once 'db_queries.php';
-//$data = (array) calculateUserRanking();
-//$firstMerge = array_merge($data, (array) calculateRanking());
-//$secondMerge = array_merge($firstMerge, (array) getTotalChanges());
 echo json_encode(getTotalChanges())."\n";
 echo json_encode(calculateRanking())."\n";
 echo json_encode(calculateUsersRanking());
