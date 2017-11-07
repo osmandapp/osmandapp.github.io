6cab3d4cbc92c19818252d93195e5f00b7d68cb1
PaulStets <stetsenko.pavlo@gmail.com>
2017-11-07 12:55:01 +0200

    Added time to the report query

--- website/reports/db_queries.php
+++ website/reports/db_queries.php
@@ -67,7 +67,7 @@ function getReport($name, $ireg = NULL) {
     return NULL;
   }
   $row = pg_fetch_all($result);
-  return json_decode((array)$row);
+  return json_encode($row);
 }
 
 function getTotalReport() {
