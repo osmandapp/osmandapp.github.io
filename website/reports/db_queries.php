f4ede2db43cf646356e7daa4a6147e89870cc4b3
PaulStets <stetsenko.pavlo@gmail.com>
2017-11-10 13:25:01 +0200

    Refresh accesstime of a report every 30 minutes

--- website/reports/db_queries.php
+++ website/reports/db_queries.php
@@ -62,20 +62,17 @@ function getReport($name, $ireg = NULL) {
     $rregion = pg_escape_string($dbconn, $ireg);
   }
   if ($month == date("Y-m")) {
-    // TODO: change the behavior of the quety depending on the accesstime
-    $accesstime = pg_query($dbconn, "select accesstime from final_reports where month = '${month}'
-          and region = '${rregion}' and name='${name}';");
-    $accesres = pg_fetch_row($accesstime);
-    if ((time() - $accessres[0])/60 > 30) {
-      pg_query($dbconn, "update final_reports set accesstime=".time()." where month = '${month}'
-          and region = '${rregion}' and name='${name}';");
-    } 
-    $result = pg_query($dbconn, "select report, time from final_reports where month = '${month}'
+    $result = pg_query($dbconn, "select report, time, accesstime from final_reports where month = '${month}'
           and region = '${rregion}' and name='${name}';");
     if (!$result) {
       return NULL;
     }
     $row = pg_fetch_row($result);
+    // set current time if a report was not accessed more than 30 minutes
+    if ((time() - $row[2])/60 > 30) {
+      pg_query($dbconn, "update final_reports set accesstime=".time()." where month = '${month}'
+          and region = '${rregion}' and name='${name}';");
+    } 
     $finalres = substr_replace($row[0], ",\"date\":\"".$row[1]."\"", strlen($row[0]) - 1, 0);
     return json_decode($finalres);
   }
