7074650a9776ea68c3c82d92f7ff212d4b6aa4d3
Victor Shcherb <victor.shcherb@gmail.com>
2017-11-11 16:20:15 +0100

    Keep access time

--- website/reports/db_queries.php
+++ website/reports/db_queries.php
@@ -139,7 +139,7 @@ function saveReport($name, $value, $month, $region = NULL, $time = 0) {
   $accesstime = 0;
   if ($result) {
     $row = pg_fetch_row($result);
-    $accesstime = $row[0];
+    $accesstime = $row[1];
   }
 
   pg_query($dbconn, "insert into final_reports(month, region, name, report, time, accesstime) 
