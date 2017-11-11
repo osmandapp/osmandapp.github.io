4a4200c8e895ec3ef014c95d3e0dcf57deb3f3a3
Victor Shcherb <victor.shcherb@gmail.com>
2017-11-11 17:08:58 +0100

    Revert testing dates

--- website/reports/db_queries.php
+++ website/reports/db_queries.php
@@ -2,10 +2,9 @@
 include_once 'db_conn.php';
 class Constants 
 { 
-  // const REFRESH_ACCESSTIME = 30 * 60; // 30 minutes
-  // const REPORTS_DELETE_DEPRECATED = 2 * 24 * 60 * 60;
-  const REFRESH_ACCESSTIME = 30; // 30 minutes
-  const REPORTS_DELETE_DEPRECATED = 50;
+  const REFRESH_ACCESSTIME = 15 * 60; // 30 minutes
+  const REPORTS_DELETE_DEPRECATED = 1 * 24 * 60 * 60; // 1 day
+  
 }
 $dbconn = db_conn();
 ini_set('display_errors', 1);
