60b7a63f099781bdbb96c3d8aeb5385adbe7f8e2
Victor Shcherb <victor.shcherb@gmail.com>
2017-11-11 17:18:57 +0100

    Update report

--- website/reports/all_contributions_requests.php
+++ website/reports/all_contributions_requests.php
@@ -1,7 +1,6 @@
 <?php
 include_once 'db_queries.php';
-echo json_encode(getTotalChanges())."\n";
-echo json_encode(calculateRanking())."\n";
-echo json_encode(calculateUsersRanking());
+echo "[" . json_encode(getTotalChanges()). ",  " . json_encode(calculateRanking()).", ". 
+		 json_encode(calculateUsersRanking()) . "]";
 
 ?>
