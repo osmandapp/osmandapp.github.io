fd3501d5a6a2363d943253da6114857303997fd9
PaulStets <stetsenko.pavlo@gmail.com>
2017-11-15 13:25:14 +0200

    Update go.php

--- website/go.php
+++ website/go.php
@@ -1,4 +1,8 @@
 <?php
 header('HTTP/1.1 302 Found');
-header('Location: http://'.$_SERVER['SERVER_NAME'].'/go.html?'.$_SERVER['QUERY_STRING']);
+if ($_SERVER['SERVER_NAME'] == 'download.osmand.net') {
+  header('Location: https://'.$_SERVER['SERVER_NAME'].'/go.html?'.$_SERVER['QUERY_STRING']);
+} else {
+  header('Location: http://'.$_SERVER['SERVER_NAME'].'/go.html?'.$_SERVER['QUERY_STRING']);
+}
 ?>
