6ef272fc045324b524a30ea784b8525595fe1c1d
PaulStets <stetsenko.pavlo@gmail.com>
2017-11-15 13:30:52 +0200

    Update go.php

--- website/go.php
+++ website/go.php
@@ -1,6 +1,7 @@
 <?php
 header('HTTP/1.1 302 Found');
-if ($_SERVER['SERVER_NAME'] == 'download.osmand.net') {
+if ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
+    || $_SERVER['SERVER_PORT'] == 443) {
   header('Location: https://'.$_SERVER['SERVER_NAME'].'/go.html?'.$_SERVER['QUERY_STRING']);
 } else {
   header('Location: http://'.$_SERVER['SERVER_NAME'].'/go.html?'.$_SERVER['QUERY_STRING']);
