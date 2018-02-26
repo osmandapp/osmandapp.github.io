3c7b07195eac3bb9be0e08a85934d1fce3fb712c
PaulStets <stetsenko.pavlo@gmail.com>
2018-02-26 18:40:38 +0200

    Update features.php

--- website/features.php
+++ website/features.php
@@ -49,8 +49,8 @@
               <ul class="articlelinklist">
                 <li><div id="google_translate_element"></div><script type="text/javascript">
                     function googleTranslateElementInit() {
-                    new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
-                 }
+                    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+                }
                 </script>
                 <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script></li>
                 <li><a data-gatag='start' href="/features?id=start">Begin with OsmAnd</a></li>
