9ceac4435511cf5979d9f1f86d72514d9a3ad2d1
Victor Shcherb <victor.shcherb@gmail.com>
2018-03-19 13:00:35 +0200

    Update website default links (move code to to the top level)

--- website/index.php
+++ website/index.php
@@ -11,21 +11,6 @@
   <script type="text/javascript" src="scripts/ga-init.js"></script>
   <script type="text/javascript" src="scripts/ga-home.js?v=2"></script>
   <link rel="stylesheet" type="text/css" href="slick.css"/>
-	<!-- Facebook Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-document,'script','https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '145552306019253'); // Insert your pixel ID here.
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=145552306019253&ev=PageView&noscript=1"
-/></noscript>
-<!-- DO NOT MODIFY -->
-<!-- End Facebook Pixel Code -->
 </head>
 <body>
 
