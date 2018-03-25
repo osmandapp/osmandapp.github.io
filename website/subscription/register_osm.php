22cdd96eea3ce1094f9c7391ada0e78af698ecce
PaulStets <stetsenko.pavlo@gmail.com>
2018-03-25 10:35:34 +0300

    Test of registration bugfix

--- website/subscription/register_osm.php
+++ website/subscription/register_osm.php
@@ -60,7 +60,7 @@
     $bitcoin = pg_escape_string($dbconn, $_POST["bitcoin_addr"]);
 //    $email = "";
     $time = time() * 1000;
-    $basic = base64_encode($osm_usr.":".$osm_pwd);
+    $basic = utf8_encode($osm_usr.":".$osm_pwd);
     $options = array(
           'http' => array(
               'header'  => "Content-type: application/x-www-form-urlencoded\r\nAuthorization: Basic {$basic}; charset=utf-8",
