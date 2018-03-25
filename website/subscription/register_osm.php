08146b0354c3d19a589b357acc4ffaa6a4a740b0
PaulStets <stetsenko.pavlo@gmail.com>
2018-03-25 11:35:55 +0300

    Test of registration bugfix

--- website/subscription/register_osm.php
+++ website/subscription/register_osm.php
@@ -56,7 +56,7 @@
     $osm_usr = pg_escape_string($dbconn, $_POST["osm_usr"]);
     $ind = strpos($osm_usr, '\'');
     if ($ind != false) {
-       $osm_usr = substr($osm_usr ,0 , $ind - 1)."\\".substr($osm_usr , $ind - 1);
+       $osm_usr = substr($osm_usr ,0 , $ind)."\\".substr($osm_usr , $ind + 1);
        echo $osm_usr;
     }
     $osm_pwd = pg_escape_string($dbconn, $_POST["osm_pwd"]);
