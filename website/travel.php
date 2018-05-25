52ea330860e2dc939d6b47f587baa292eb1995cb
PaulStets <stetsenko.pavlo@gmail.com>
2018-05-25 09:51:26 +0300

    Update travel.php

--- website/travel.php
+++ website/travel.php
@@ -1,4 +1,4 @@
 <?php
-    header("Location: https://".$_GET["lang"].".wikivoyage.org/wiki/".$_GET["title"]);
+    header("Location: https://www.".$_GET["lang"].".wikivoyage.org/wiki/".$_GET["title"], true, 302);
     exit;
 ?>
