2b7929beb11e4ab5991ce794373bf7910d51d2f7
PaulStets <stetsenko.pavlo@gmail.com>
2018-06-05 16:51:10 +0300

    Added script to insert into email_unsubscribed

--- website/unsubscribe.php
+++ website/unsubscribe.php
@@ -1,6 +1,9 @@
 <?php
 include 'db_queries.php';
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 insertIntoUnsubscribed(base64_decode( urldecode( $_GET["id"] ) ), $_GET["group"]);
 
 ?>
