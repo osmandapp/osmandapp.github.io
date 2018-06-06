b6df2b744d4d7cdfb32f27d9d0ab417bb58cdd7d
PaulStets <stetsenko.pavlo@gmail.com>
2018-06-06 09:52:20 +0300

    Update db_queries.php

--- website/reports/db_queries.php
+++ website/reports/db_queries.php
@@ -905,8 +905,8 @@ function insertIntoUnsubscribed($email, $channel) {
   global $dbconn;
   
   $timestamp = time();
-  pg_query($dbconn, "insert or ignore into email_unsubscribed(email, channel, timestamp) 
-      values('${email}', '${channel}', $timestamp );");
+  pg_query($dbconn, "insert into email_unsubscribed(email, channel, timestamp) 
+      values('${email}', '${channel}', $timestamp ) on conflict email do nothing;");
 }
 
 ?>
