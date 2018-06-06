7288a0d047b3343ca106843f895acca46dfead3c
PaulStets <stetsenko.pavlo@gmail.com>
2018-06-06 09:47:42 +0300

    Update unsubscribe.php

--- website/unsubscribe.php
+++ website/unsubscribe.php
@@ -3,7 +3,7 @@ include 'reports/db_queries.php';
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
+echo ("You have sucessfully unsubscribed from OsmAnd's mailing list.";
 insertIntoUnsubscribed(base64_decode( urldecode( $_GET["id"] ) ), $_GET["group"]);
 
 ?>
