e16c3338baa9a9ad0a1324828330f97d0c774f09
Dmitriy Prodchenko <dmpr0.ua@gmail.com>
2018-06-06 11:52:39 +0300

    Add styling for Unsubscribe page

--- website/unsubscribe.php
+++ website/unsubscribe.php
@@ -1,6 +1,10 @@
 <?php
 include 'reports/db_queries.php';
-echo ("You have sucessfully unsubscribed from OsmAnd's mailing list.");
 insertIntoUnsubscribed(base64_decode( urldecode( $_GET["id"] ) ), $_GET["group"]);
 
 ?>
+
+<div style="width:75%; margin: auto; padding-top: 2%; background:#ffffff;">
+    <img style="display: block; margin-left: auto; margin-right: auto;" src="/images/logo-grey.png"/>
+    <p style="font-family: sans-serif; font-size: 18px; text-align: center;">You have sucessfully unsubscribed from OsmAnd's mailing list.</p>
+</div>
