8f97fb77aa47fd9d5de8b6f76dca7e51b1b10b0c
PaulStets <stetsenko.pavlo@gmail.com>
2018-06-08 11:26:11 +0300

    Update unsubscribe.php

--- website/unsubscribe.php
+++ website/unsubscribe.php
@@ -1,6 +1,13 @@
 <?php
 include 'reports/db_queries.php';
-insertIntoUnsubscribed(base64_decode( urldecode( $_GET["id"] ) ), $_GET["group"]);
+    error_reporting(E_ALL);
+ini_set('display_errors', 1);
+    if($_SERVER['SERVER_NAME'] == 'builder.osmand.net') {
+        insertIntoUnsubscribed(base64_decode( urldecode( $_GET["id"] ) ), $_GET["group"]);
+    }
+    else {
+        file_get_contents("http://builder.osmand.net/unsubscribe?".$_SERVER['QUERY_STRING']);
+    }
 ?>
 
 <div style="width:75%; margin: auto; padding-top: 2%; background:#ffffff;">
