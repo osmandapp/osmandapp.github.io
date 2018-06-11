112a73918fa847d194d16f82759a4842af7c253f
annasoloviova <annasoloviova@users.noreply.github.com>
2018-06-11 15:53:35 +0300

    Update blog.php

--- website/blog.php
+++ website/blog.php
@@ -61,7 +61,7 @@
           <div class="acticlestitles">
             <h2>Latest articles</h2>
             <ul class="articlelinklist">
-              <li><a data-gatag='travel' href="/blog?id=8-years">Travel guides</a></li>
+              <li><a data-gatag='travel' href="/blog?id=travel">Travel guides</a></li>
               <li><a data-gatag='8-years' href="/blog?id=8-years">OsmAnd turns 8</a></li>
               <li><a data-gatag='osmand-3-0-released' href="/blog?id=osmand-3-0-released">OsmAnd 3.0</a></li>
               <li><a data-gatag='osmand-ios-2-1-released' href="/blog?id=osmand-ios-2-1-released">OsmAnd 2.1 (iOS)</a></li>
