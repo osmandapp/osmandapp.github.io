49f21be41046646e84b502f3abf5509f0623534d
Victor Shcherb <victor.shcherb@gmail.com>
2018-06-12 16:11:13 +0200

    Update motd

--- website/api/motd.php
+++ website/api/motd.php
@@ -1,6 +1,8 @@
 <?php
-if(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] == '82.217.128.95') {
-	echo file_get_contents("messages/test_world_cup.json");	
+// lang, version
+if(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] == '82.217.128.95' && 
+   isset($_REQUEST['version']) && substr($_REQUEST['version'] , 0, 1 ) == '3' ) {
+	echo file_get_contents("messages/top_wikivoyage.json");	
 } else if(isset($_GET['os']) && $_GET['os'] == 'ios') {
   // url supported: 
   // 1. screen in app [screen:in_apps], [in_app:<inappid>]
