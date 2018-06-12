8d0e19cb661d80ce26bce5439862d5ec79737665
Victor Shcherb <victor.shcherb@gmail.com>
2018-06-12 15:58:58 +0200

    Test motd world cup

--- website/api/motd.php
+++ website/api/motd.php
@@ -1,14 +1,16 @@
 <?php
-if(isset($_GET['os']) && $_GET['os'] == 'ios') {
+if(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] == '82.217.128.95') {
+	echo file_get_contents("messages/test_world_cup.json");	
+} else if(isset($_GET['os']) && $_GET['os'] == 'ios') {
   // url supported: 
   // 1. screen in app [screen:in_apps], [in_app:<inappid>]
   // 2. http url supported
-  echo file_get_contents("./discount_ios.json");
+  echo file_get_contents("messages/discount_ios.json");
 } else {
  // url supported: 
  // 1. "osmand-market-app:net.osmand.plus" - market redirect
  // 2. http url supported
-  echo file_get_contents("./discount.json");
+  echo file_get_contents("messages/discount.json");
 }
 
 ?>
