ec02f4ee10de00cb9cd63486e670e48212a7d3a2
Dmitriy Prodchenko <dmpr0.ua@gmail.com>
2018-07-17 13:03:56 +0300

    Change date for Sale.

--- website/api/motd.php
+++ website/api/motd.php
@@ -1,7 +1,7 @@
 <?php
 // lang is also passwed
-$DISCOUNT_START = "2018-06-21 00:00";
-$DISCOUNT_END = "2018-06-26 00:00";
+$DISCOUNT_START = "2018-07-25 00:00";
+$DISCOUNT_END = "2018-07-31 00:00";
 
 $TEST_IP = '82.217.128.95';
 
