b0710ad72ddf36a807b1831a253f9016181000b0
vshcherb <victor.shcherb@gmail.com>
2018-07-23 09:44:34 +0200

    Update geo-ip.php

--- website/api/geo-ip.php
+++ website/api/geo-ip.php
@@ -1,6 +1,6 @@
 <?php
  $obj = json_decode(file_get_contents("http://freegeoip.net/json/".$_SERVER['REMOTE_ADDR']));
- if(isset($obj->lat) && isset($obj->lon) && !isset($obj->latitude) && !isset($obj->longitude)) {
+ if(isset($obj['lat']) && isset($obj['lon']) && !isset($obj['latitude']) && !isset($obj['longitude'])) {
     $obj->latitude = $obj->lat;
     $obj->longitude = $obj->lon;
  }
