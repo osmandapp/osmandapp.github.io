fdc96c70724a0416bd0057de72765eaafc1e8f4e
vshcherb <victor.shcherb@gmail.com>
2018-07-23 09:35:04 +0200

    Update geo-ip.php

--- website/api/geo-ip.php
+++ website/api/geo-ip.php
@@ -1,3 +1,9 @@
 <?php
- echo file_get_contents("http://freegeoip.net/json/".$_SERVER['REMOTE_ADDR']);
+ $obj = json_decode(file_get_contents("http://freegeoip.net/json/".$_SERVER['REMOTE_ADDR']));
+ if($obj->lat  && $obj->lon && !obj->latitude && !obj->longitude) {
+    $obj->latitude = $obj->lat;
+    $obj->longitude = $obj->lon;
+ }
+  
+ echo json_encode($obj);
 ?>
