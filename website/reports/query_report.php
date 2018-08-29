9e81cf676d6f838303181d085efcb87cbf498d7f
Victor Shcherb <victor.shcherb@gmail.com>
2018-08-29 18:07:56 +0200

    Update reports

--- website/reports/query_report.php
+++ website/reports/query_report.php
@@ -1,9 +1,3 @@
 <?php
-//if($_SERVER['SERVER_NAME'] == 'builder.osmand.net') {
-	$ctx = stream_context_create(array('http'=> array('timeout' => 600)  ));
-	$get_result = file_get_contents("http://localhost:".$_SERVER['SERVER_PORT']."/reports/".$_GET["report"].".php?".$_SERVER['QUERY_STRING'], false, $ctx);
-	echo $get_result;
-//	die;
-//}
-//echo file_get_contents("http://builder.osmand.net/reports/query_report.php?".$_SERVER['QUERY_STRING']);
+include $_GET["report"].".php"	
 ?>
