e834c7f9d60bb58ece23850de4aa7248711f2752
Victor Shcherb <victor.shcherb@gmail.com>
2018-09-04 15:29:21 +0200

    Delete php

--- website/go.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('HTTP/1.1 302 Found');
-if ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
-    || $_SERVER['SERVER_PORT'] == 443) {
-  header('Location: https://'.$_SERVER['SERVER_NAME'].'/go.html?'.$_SERVER['QUERY_STRING']);
-} else {
-  header('Location: http://'.$_SERVER['SERVER_NAME'].'/go.html?'.$_SERVER['QUERY_STRING']);
-}
-?>
--- website/travel.php
+++
@@ -1,4 +0,0 @@
-<?php
-    header("Location: https://".$_GET["lang"].".wikivoyage.org/wiki/".$_GET["title"], true, 302);
-    exit;
-?>
